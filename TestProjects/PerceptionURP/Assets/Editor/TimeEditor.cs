b7391b9c9468c98357218e7298ccbd1ef20cb3f3
Jon Hogins <jonathanh@unity3d.com>
2020-06-24 11:44:43 -0700

    Updating project and adding test scripts

---
+++ TestProjects/PerceptionURP/Assets/Editor/TimeEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[InitializeOnLoad]
+public class TimeEditor
+{
+    static TimeEditor()
+    {
+        Debug.Log("Timer: Startup. " + Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency);
+        EditorApplication.playModeStateChanged += change => Debug.Log("Timer: Changing playmode. " + Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency);
+    }
+}
---
+++ TestProjects/PerceptionURP/Assets/EndAfter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAfter : MonoBehaviour
+{
+    public int totalFrames = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount == totalFrames)
+        {
+            Debug.Log($"total time: {Time.realtimeSinceStartup}  FPS: {Time.frameCount / Time.realtimeSinceStartup}");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
